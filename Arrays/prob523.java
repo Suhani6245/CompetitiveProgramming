026438d6f5cf73dfd3aaca9e6da4eb6c889e6445
Suhani <su1106hani@gmail.com>
2026-06-24 12:57:53 +0530

    24 june

---
+++ Arrays/prob523.java
@@ -0,0 +1,20 @@
+//import java.util.*;
+public class prob523 {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int k = 5;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum % k == 0) {
+                    System.out.print("Subarray [" + i + "," + j + "]: ");
+                    for (int t = i; t <= j; t++) {
+                        System.out.print(arr[t] + (t == j ? "" : " "));
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
---
+++ Arrays/subarrays.java
@@ -0,0 +1,31 @@
+//import java.util.*;
+
+public class subarrays {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        // for(int i=0; i<arr.length; i++){
+        //     for(int j=i; j<arr.length; j++){
+        //         System.out.print("[");
+        //         for (int k = i; k <= j; k++) {
+        //             System.out.print(arr[k] + (k == j ? "" : ", "));
+        //         }
+        //         System.out.println("]");
+        //     }
+        // }
+
+        //while
+        int i = 0;
+        while (i < arr.length) {
+            int j = i;
+            while (j < arr.length) {
+                System.out.print("[");
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + (k == j ? "" : ", "));
+                }
+                System.out.println("]");
+                j++;
+            }
+            i++;
+        }
+    }
+}
